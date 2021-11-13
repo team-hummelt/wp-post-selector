7d498f1be74935d9b77e5dff75fba8af483a60ee
Jens Wiecker <jens@harz-web.com>
2021-11-13 22:40:15 +0100

    Rev v1.0.6

--- wp-post-selector.php
+++ wp-post-selector.php
@@ -10,14 +10,14 @@
  * Plugin Name:       Post-Selector - Gutenberg Block Plugin
  * Plugin URI:        https://www.hummelt-werbeagentur.de/leistungen/
  * Description:       Auswahl von Beiträgen im Gutenberg-Block Editor mit verschiedenen Ausgabeoptionen.
- * Version:           1.0.5
+ * Version:           1.0.6
  * Author:            Jens Wiecker
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Requires PHP:      8.0
  * Requires at least: 5.8
  * Tested up to:      5.8
- * Stable tag:        1.0.5
+ * Stable tag:        1.0.6
  */
 
 defined( 'ABSPATH' ) or die();
